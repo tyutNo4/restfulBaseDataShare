8dbe4f420c73e5b3cce2991e0a291cc4e096b0c8
ChivalrousDig <ChivalrousDig@gmail.com>
2012-08-11 22:55:53 +0800

    http鉴权客户端生成

---
+++ src/test/java/test/ConnectToUrlUsingBasicAuthentication.java
@@ -0,0 +1,51 @@
+package test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class ConnectToUrlUsingBasicAuthentication {
+
+	public static void main(String[] args) {
+
+		try {
+			String webPage = "http://localhost:8080/centralizeManage/rest/kfc/brands/sdfsdf";
+			String name = "mkyong";
+			String password = "123456";
+
+			String authString = name + ":" + password;
+			System.out.println("auth string: " + authString);
+			byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+			String authStringEnc = new String(authEncBytes);
+			System.out.println("Base64 encoded auth string: " + authStringEnc);
+
+			URL url = new URL(webPage);
+			URLConnection urlConnection = url.openConnection();
+			urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+			InputStream is = urlConnection.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+
+			int numCharsRead;
+			char[] charArray = new char[1024];
+			StringBuffer sb = new StringBuffer();
+			while ((numCharsRead = isr.read(charArray)) > 0) {
+				sb.append(charArray, 0, numCharsRead);
+			}
+			String result = sb.toString();
+
+			System.out.println("*** BEGIN ***");
+			System.out.println(result);
+			System.out.println("*** END ***");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
